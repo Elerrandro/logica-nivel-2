1df035a7ac2e2ababaa7462f0585b89245c1e1d0
Elerrandro <elerrandrosousa@gmail.com>
2024-08-30 20:56:08 -0300

    Exercícios

---
+++ 100 Exercícios/66.py
@@ -0,0 +1,6 @@
+#66) O mesmo exercício anterior, mas agora, considere que o segundo valor lido poderá ser maior ou menor que o primeiro valor lido, ou seja, deve-se testá-los.
+
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite outro valor: "))
+soma = valor1 + valor2
+print(f"A soma do {valor1} e {valor2} resulta em: {soma}")
