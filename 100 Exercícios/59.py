6857d35cbb17b15f4ef5e4e55f9b9fe8f80eae12
Elerrandro <elerrandrosousa@gmail.com>
2024-08-30 19:37:17 -0300

    Update 59.py

--- 100 Exercícios/59.py
+++ 100 Exercícios/59.py
@@ -1,1 +1,11 @@
 #59) Ler 10 valores e escrever quantos desses valores lidos são NEGATIVOS.
+
+valores = []
+
+for x in range(1, 11):
+    valores.append(int(input("Insira um valor inteiro: ")))
+
+#FILTRA SOMENTE OS NÚMEROS NEGATIVOS
+negativos = list(filter(lambda x: x < 0, valores))
+#Len irá ler quantos números tem na variavel
+print(f"Dos 10 valores {len(negativos)} São negativos !")
