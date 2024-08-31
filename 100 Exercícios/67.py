58adf617cd776119f561377fe4a97b1f09c7eebf
Elerrandro <elerrandrosousa@gmail.com>
2024-08-30 21:19:53 -0300

    Exercícios

---
+++ 100 Exercícios/67.py
@@ -0,0 +1,6 @@
+#67) Faça um algoritmo que calcule e escreva a média aritmética dos números inteiros entre 15 (inclusive) e 100 (inclusive).
+
+x = 100 - 15 + 1
+soma = x * (15+100) / 2
+media = soma / x
+print(f"A média é {media}")
