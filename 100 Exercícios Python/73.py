70fe9eb3bc9736477bb95b4c42d75e5d22c64d81
Elerrandro <elerrandrosousa@gmail.com>
2024-09-06 19:50:02 -0300

    Update 73.py

--- 100 Exercícios Python/73.py
+++ 100 Exercícios Python/73.py
@@ -10,41 +10,43 @@
 lista_nome = []
 lista_salario = []
 lista_filhos = []
-
-populacao = int(input("Nessa Pesquisa, quantas pessoas vão ser entrevistadas: "))
-
-for x in range(populacao):
-  nome = str(input("Insira o seu nome: "))
-  salario = float(input("Insira o seu sálario: "))
-  filhos = int(input("Insira quantos filhos tem: "))
-  
-  lista_nome.append(nome)
-  lista_salario.append(salario)
-  lista_filhos.append(filhos)
-
-#(A)media do sálario da população
-valor_total = sum(lista_salario)
-media_salario = valor_total / len(lista_salario)
-
-
-#(B)média de filhos da população
-filhos_total = sum(lista_filhos)
-media_filho = filhos_total / len(lista_filhos)
-
-  
-#(C)o maior sálario entre a população
-maior_salario =  max(lista_salario)
-
-
-#(D)Percentual de pessoas com salário menor de R$150,00
-pessoas_menor_150 = list(filter(lambda x: x < 150, lista_salario))
-total_pessoas = len(lista_salario)
-pessoas_salario_menor_150 = len(pessoas_menor_150)
-percentual = (pessoas_salario_menor_150 / total_pessoas) * 100
-
-print("-----" * 30)
-
-print(f"A média do sálario da população é: R${media_salario:.2f}")
-print(f"A média de filhos da população é: {media_filho:.0f}")
-print(f"O maior sálario entre a população é: R${maior_salario:.2f}")
-print(f"O percentual de pessoas com salário menor que R$150,00 é: {percentual:.2f}%")
+while True:
+  populacao = int(input("Nessa Pesquisa, quantas pessoas vão ser entrevistadas: "))
+  if populacao <= 0:
+    break
+  else:
+    for x in range(populacao):
+      nome = str(input("Insira o seu nome: "))
+      salario = float(input("Insira o seu sálario: "))
+      filhos = int(input("Insira quantos filhos tem: "))
+    
+      lista_nome.append(nome)
+      lista_salario.append(salario)
+      lista_filhos.append(filhos)
+    
+    #(A)media do sálario da população
+    valor_total = sum(lista_salario)
+    media_salario = valor_total / len(lista_salario)
+    
+    
+    #(B)média de filhos da população
+    filhos_total = sum(lista_filhos)
+    media_filho = filhos_total / len(lista_filhos)
+    
+    
+    #(C)o maior sálario entre a população
+    maior_salario =  max(lista_salario)
+    
+    
+    #(D)Percentual de pessoas com salário menor de R$150,00
+    pessoas_menor_150 = list(filter(lambda x: x < 150, lista_salario))
+    total_pessoas = len(lista_salario)
+    pessoas_salario_menor_150 = len(pessoas_menor_150)
+    percentual = (pessoas_salario_menor_150 / total_pessoas) * 100
+    
+    print("-----" * 30)
+    
+    print(f"A média do sálario da população é: R${media_salario:.2f}")
+    print(f"A média de filhos da população é: {media_filho:.0f}")
+    print(f"O maior sálario entre a população é: R${maior_salario:.2f}")
+    print(f"O percentual de pessoas com salário menor que R$150,00 é: {percentual:.2f}%")
